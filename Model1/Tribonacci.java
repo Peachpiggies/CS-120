bc43fb0ebd3346262d88adb29fed0b539875ff6f
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-02-24 12:03:29 -0400

    New code that sill in editting process.

---
+++ Model1/Tribonacci.java
@@ -0,0 +1,29 @@
+/*
+
+  * Add variables
+  * Make a sequence of Tribonacci [T(n)=T(n-1)+T(n-2)+T(n-3), T(1)=T(2)=1, and T(3)=2]
+  * Print the out put
+
+*/
+
+public class Tribonacci {
+
+    public static void main(String[] args) {
+
+        int n, // n is n in the Tribonacci
+            num, /* num will be the subtraction of n*/
+            sequence /* will be the variable to send output */;
+
+        n = 1;
+        num = 1;
+
+        while (num <= 20) {
+
+            sequence = n - num;
+            
+
+        }
+
+    }
+    
+}
