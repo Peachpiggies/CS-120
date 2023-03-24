40c639b8cef2aeb558a489f6d3ac9a954d3e2235
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-03-24 12:04:49 -0300

    New project coming up soon...

---
+++ TimeTable.java
@@ -0,0 +1,19 @@
+public class TimeTable {
+    
+    public static void main(String[] args) {
+
+        int row, col;
+
+        for (row = 1; row <= 7; row++) {
+    
+            for (col = 1; col <= 31; col++) {
+    
+                
+    
+            }
+    
+        }
+
+    }
+
+}
