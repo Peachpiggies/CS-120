8e139f0458c8a1de3699af941835b3b2695bb896
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-04-13 11:21:07 -0300

    New project that will check vowels & digits.

---
+++ Model3/CheckVowelsDigits.java
@@ -0,0 +1,18 @@
+/*
+
+  * prompts the user for a String, counts the number of vowels (a, e, i, o, u, A, E, I, O, U) and digits (0-9) contained in the string, and prints the counts and the percentages (with 2 decimal digits)
+  
+  - import java.util.Scanner to recive information from keyboard
+  - use in.next().toLowerCase() to convert the input String to lowercase
+
+*/
+
+public class CheckVowelsDigits {
+
+    public static void main(String[] args) {
+
+        String text;
+
+    }
+
+}
