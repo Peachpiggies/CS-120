e58630c55d649d17a16aee38934748fc303a5cfc
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-04-17 11:27:08 -0300

    New project

---
+++ Model3/PhoneKeyPad.java
@@ -0,0 +1,24 @@
+/*
+
+ * On your phone keypad, the alphabets are mapped to digits as follows: 
+    ABC(2), 
+    DEF(3), 
+    GHI(4), 
+    JKL(5), 
+    MNO(6), 
+    PQRS(7), 
+    TUV(8), 
+    WXYZ(9).
+   Write a program called PhoneKeyPad, which prompts user for a String (case insensitive), and converts to a sequence of keypad digits.
+ 
+*/
+
+public class PhoneKeyPad {
+
+    public static void main(String[] args) {
+
+        
+
+    } 
+
+}
