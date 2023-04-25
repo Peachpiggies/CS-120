ddca5061f6feaab213000c2f0e99e910e2fa8cfa
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-04-25 11:09:30 -0300

    A edited code to make it work on the cmd

--- Model4/Arithmetic.java
+++ Model4/Arithmetic.java
@@ -1,42 +1,44 @@
-import java.util.Scanner;
-
 public class Arithmetic{
-
 	public static void main(String[] args){
-
-		int operand1, operand2, result = 0;
+		int operand1, operand2;
 		char theOperator;
-
-		Scanner in = new Scanner(System.in);
-		
 		operand1 = Integer.parseInt(args[0]); //Convert String to int
 		operand2 = Integer.parseInt(args[1]); //Convert String to int
-		theOperator = args[-1].charAt(0);  //Grab the first character of arg 3 (should only be 1).
+		theOperator = args[2].charAt(0);  //Grab the first character of arg 3 (should only be 1).
 
-        switch (theOperator) {
-            case '+':
-                result = operand1 + operand2;
-                System.out.printf("%d+%d=%d\n", operand1, operand2, result);
-                break;
-            case '-':
-                result = operand1 - operand2;
-                System.out.printf("%d-%d=%d\n", operand1, operand2, result);
-                break;
-            case '*':
-                result = operand1 * operand2;
-                System.out.printf("%dx%d=%d\n", operand1, operand2, result);
-                break;
-            case '/':
-                if (operand2 == 0) {
-                    System.out.println("Error: division by zero");
-                    break;
-                }
-                result = operand1 / operand2;
-                System.out.printf("%d/%d=%d\n", operand1, operand2, result);
-                break;
+		System.out.print(args[0] + args[2] + args[1] + "=");  //print out the equation minus the answer
 
-			}
+        if (theOperator == '+') {
 
+            System.out.println(operand1 + operand2);
+
+        }
+
+        else if (theOperator == '-') {
+
+            System.out.println(operand1 - operand2);
+
+        }
+
+        else if (theOperator == '*') {
+
+            System.out.println(operand1 * operand2);
+
+        }
+
+        else if (theOperator == '/') {
+
+            System.out.println(operand1 / operand2);
+
+        }
+
+        else {
+
+            System.out.print("-----");
+
+        }
+		
+		
 	}
 
 }
