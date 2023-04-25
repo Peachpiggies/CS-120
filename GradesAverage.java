0a63d3b02626058c1d5fd0dfc0630b1b05859dec
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-04-25 12:01:05 -0300

    Still working on it

---
+++ GradesAverage.java
@@ -0,0 +1,30 @@
+/* 
+
+    * prompts user for the number of students, reads it from the keyboard, and saves it in an int variable called numStudents. 
+      It then prompts user for the grades of each of the students and saves them in an int array called grades.  
+      Your program shall check that the grade is between 0 and 100.
+
+    * import Scanner
+
+ */
+
+import java.util.Scanner;
+
+public class GradesAverage {
+
+    int numStudents, grade, GradesAverage;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of students : ");
+
+    }
+
+    public static float grade(float GradesAverage) {
+
+        
+
+    }
+    
+}
