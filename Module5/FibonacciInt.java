fe6d90581cdc9a902b500da35b077db9eedfe3a4
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-05-10 11:11:51 -0300

    Move & edit some code

--- Module5/FibonacciInt.java
+++ Module5/FibonacciInt.java
@@ -9,19 +9,14 @@ public class FibonacciInt {
 
     public static void main(String[] args) {
 
-        int n1, /* n1 is the frist number in the Fibonacci sequence */
-            n2, /* n2 is the second number in the Fibonacci sequence */
-            count, /* this variable will start at 0 until 20 in the while statement */
+        int n1 = 1, /* n1 is the frist number in the Fibonacci sequence */
+            n2 = 1, /* n2 is the second number in the Fibonacci sequence */
+            count = 2, /* this variable will start at 0 until 20 in the while statement */
             sequence, /* will be the variable to send output */ 
             i = 0,
             n = i;
 
         double  sum = 2; /* sum of first two numbers (1+1) */
-
-
-        n1 = 1;
-        n2 = 1;
-        count = 2;
 
         System.out.printf("f(%s) = %d\nf(%s) = %d", i, n1, n + 1, n2);
 
