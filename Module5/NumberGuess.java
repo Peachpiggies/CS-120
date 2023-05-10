a254e2bfdd3e184c43a735e1600481bd8c6f4736
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-05-10 11:50:44 -0300

    finish the code

--- Module5/NumberGuess.java
+++ Module5/NumberGuess.java
@@ -1,7 +1,9 @@
 /*
 
  * import scanner to get input
- * make a while loop
+ * make a if-else statement in the while loop to check & count the number
+ * use if statements to check the number variable that is correct with the secret_number variable
+   then print out the counter variable that have count from the if-else statement from the while loop
 
 */
 
@@ -12,33 +14,38 @@ public class NumberGuess {
     public static void main(String[] args) {
 
         int secret_number = (int)(Math.random() * 100),
-        number;
+            number,
+            counter = 1;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Key in your guess");
+        System.out.print("Key in your guess : ");
         number = input.nextInt();
 
         while (number != secret_number) {
 
             if (number > secret_number) {
 
-                System.out.print("Try lower...");
-                break;
+                System.out.print("Try lower...\n");
+                number = input.nextInt();
+                counter += 1;
+                //break;
 
             }
 
             else if (number < secret_number) {
 
-                System.out.print("Try higher...");
-                break;
+                System.out.print("Try higher...\n");
+                number = input.nextInt();
+                counter += 1;
+                //break;
 
             }
-            else {
 
-                System.out.print("you got it");
-                break;
+        }
 
-            }
+        if (number == secret_number) {
+
+            System.out.printf("You got it in %d trials!", counter);
 
         }
 
