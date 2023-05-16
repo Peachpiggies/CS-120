277a5113b988f7491a643bbe0e662b536052f9f9
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-05-16 11:07:25 -0300

    Still planning

---
+++ Test 2/ChineseZodiac.java
@@ -0,0 +1,16 @@
+/*
+ * import Scanner
+*/
+
+import java.util.Scanner;
+
+public class ChineseZodiac {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+
+    }
+    
+}
