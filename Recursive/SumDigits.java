8b966c94b0bbf77649973640053d243c24b9626b
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-05-30 12:04:32 -0300

    Working on it...

---
+++ Recursive/SumDigits.java
@@ -0,0 +1,17 @@
+/*
+ * import Scanner
+ * make a recursive function that calculate, but before that have to check the digits befor calculated
+ * print the output in main function
+*/
+
+import java.util.Scanner;
+
+public class SumDigits {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+    
+}
