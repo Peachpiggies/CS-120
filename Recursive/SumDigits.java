f9030ccca6c0459c49df1a0752f02303f37f5859
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-05-31 09:15:02 -0300

    Done.

--- Recursive/SumDigits.java
+++ Recursive/SumDigits.java
@@ -1,6 +1,8 @@
 /*
  * import Scanner
  * make a recursive function that calculate, but before that have to check the digits befor calculated
+ * in the calculate function I use if-else aubeously it's recursive function 1st if check it's only a digit
+ * and the else it use for calculate
  * print the output in main function
 */
 
@@ -10,7 +12,32 @@ public class SumDigits {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
         
+        System.out.print("Enter the number to sum the digits of :");
+        int number = input.nextInt(),
+            sum = calculate(number);
+
+        System.out.printf("The sum of the digits %s is %s", number, sum);
+
+    }
+
+    public static int calculate(int number) {
+
+        if (number < 10) {
+
+            return number; // Base case: single-digit number
+
+        } 
+        
+        else {
+
+            int lastDigit = number % 10, // Get the last digit
+                remainingDigits = number / 10; // Remove the last digit
+
+            return lastDigit + calculate(remainingDigits);
+
+        }
 
     }
     
