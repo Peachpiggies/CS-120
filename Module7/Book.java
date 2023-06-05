21150079f550443cd8ea860f1ebf860259e703d3
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-06-05 09:11:15 -0300

    Make public methods.

--- Module7/Book.java
+++ Module7/Book.java
@@ -2,8 +2,8 @@
  * A class called Book is designed (as shown in the class diagram) to model a book written by one author. It contains:
     •	Four private instance variables: name (String), author (of the class Author you have just created, assume that a book has one and only one author), price (double), and qty (int);
     •	Two constructors:
-    •	public Book (String name, Author author, double price) { ...... }
-        public Book (String name, Author author, double price, int qty) { ...... }
+        •	public Book (String name, Author author, double price) { ...... }
+            public Book (String name, Author author, double price, int qty) { ...... }
     •	public methods getName(), getAuthor(), getPrice(), setPrice(), getQty(), setQty().
     •	A toString() that returns "Book[name=?,Author[name=?,email=?,gender=?],price=?,qty=?".  You should reuse Author’s toString().
 Write the Book class (which uses the Author class written earlier). Also write a test driver called TestBook to test all the public methods in the class Book. Take Note that you have to construct an instance of Author before you can construct an instance of Book. E.g.,
@@ -18,6 +18,34 @@ public class Book {
 
     private double price;
 
-    private int qty;
+    private int qty = 0;
+
+    public Book ( String name, Author author, double price ) {}
+
+    public Book ( String name, Author author, double price, int qty ) {}
+
+    public String getName() {
+        
+        return name; 
+    
+    }
+
+    public Author getAuthor() {
+
+        return author;
+
+    }
+
+    public double getPrice() {
+
+        return price;
+
+    }
+
+    public Book( int qty ) {
+
+        this.qty = qty;
+
+    }
     
 }
