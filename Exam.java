6707a944cbafbe18cf70c65a2e4a97c65795ba0e
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-06-14 09:27:37 -0300

    Exam

---
+++ Exam.java
@@ -0,0 +1,17 @@
+// Start without using the scanner becuz I need the input on cmd
+// make that the start and end from the cmd input should be 3 numbers apart
+// print the number that we check that it's in the middle of the start and end as a ignore int
+// print the sum of start and end
+
+public class Exam {
+
+    public static void main(String[] args) {
+
+        String start = args[0];
+        String end = args[-1];
+
+        if ( int start + 3 == int end ) {}
+
+    }
+
+}
